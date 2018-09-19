2e24e52db1eb8d906e81e7db58cb2f380285aa6a
Oliver Monberg <au554208@uni.au.dk>
2018-09-19 07:49:24 +0200

    Add project files.

---
+++ AgentAssignment/Agent.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace AgentAssignment
+{
+    public class Agents : ObservableCollection<Agent> {
+
+        Agents()
+        {
+            Add(new Agent("007", "James Bond", "Assassination", "UpperVolta"));
+            Add(new Agent("006", "Bames Jond", "Baker", "Ukraine"));
+        }
+
+    };  // Just to reference it from xaml
+
+    [Serializable]
+    public class Agent
+    {
+        string id;
+        string codeName;
+        string speciality;
+        string assignment;
+
+        public Agent()
+        {
+        }
+
+        public Agent(string aId, string aName, string aSpeciality, string aAssignment)
+        {
+            id = aId;
+            codeName = aName;
+            speciality = aSpeciality;
+            assignment = aAssignment;
+        }
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string CodeName
+        {
+            get
+            {
+                return codeName;
+            }
+            set
+            {
+                codeName = value;
+            }
+        }
+
+        public string Speciality
+        {
+            get
+            {
+                return speciality;
+            }
+            set
+            {
+                speciality = value;
+            }
+        }
+
+        public string Assignment
+        {
+            get
+            {
+                return assignment;
+            }
+            set
+            {
+                assignment = value;
+            }
+        }
+    }
+}
